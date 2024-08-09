8087b860c3285db6c1a440633984b38ac3fa290e
brian <liar88828@gmail.com>
2024-08-09 19:29:13 +0700

    init

---
+++ app/src/main/java/com/tutor/wallet_app/ui/theme/Font.kt
@@ -0,0 +1,26 @@
+package com.tutor.wallet_app.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.tutor.wallet_app.R
+
+val fontPoppins = FontFamily(
+	Font(
+		R.font.poppins,
+		weight = FontWeight.Normal,
+		style = FontStyle.Normal
+	),
+	Font(
+		R.font.poppins_light,
+		weight = FontWeight.Light,
+		style = FontStyle.Normal
+	),
+	Font(
+		R.font.poppins_bold,
+		weight = FontWeight.Bold,
+		style = FontStyle.Normal
+	)
+
+)
